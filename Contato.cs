1b793ecfdc440b3d075c830e1b3509c6f713b616
randrade-trends <randrade@tecnotrends.com.br>
2026-01-22 12:24:01 -0300

    Create Contato.cs

---
+++ Contato.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agendaADONET.Classe
+{
+    public class Contato
+    {
+        public int id{get; set;}
+        public string nome { get; set; }
+        public string email { get; set; }
+        public int myProperty { get; set; }
+
+
+    }
+}
